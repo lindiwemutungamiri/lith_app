47a7bd3ed476fda69d1bc0cb8431157abd4e9afc
Lindiwe Mutungamiri <lindiwemutungamiri23@gmail.com>
2022-05-06 01:13:31 +0000

    added voice calling and video calling

--- lib/screens/patient_dashboard/doctor_appointment/components/agora.config.dart
+++ lib/screens/patient_dashboard/doctor_appointment/components/agora.config.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:lith_app/screens/patient_dashboard/doctor_appointment/screens/token_maker.dart';
 
 /// Get your own App ID at https://dashboard.agora.io/
-String appId = env['AGORA_APP_ID'].toString();
+String appId = env['69c668730c63437e95c637594e8297b7'].toString();
 
 /// Please refer to https://docs.agora.io/en/Agora%20Platform/token
 String token = updatedtoken;
